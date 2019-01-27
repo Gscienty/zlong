c089cc96d0179b82c61fe4f2335c42d44a232b1c
高小川 <gaoxiaochuan@hotmail.com>
2019-01-27 22:52:26 +0800

    fix container_of bug

--- include/utils/define.h
+++ include/utils/define.h
@@ -2,6 +2,6 @@
 #define _ZL_UTILS_DEFINE_H
 
 #define container_of(ptr, type, member) \
-    ((type *) ((void *) ptr - (void *)(&((type *) 0)->member - 0)))
+    ((type *) ((void *) (ptr) - (void *)(&((type *) 0)->member - 0)))
 
 #endif
