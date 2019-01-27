976ad2a14895bdc86f5f7b747925ef49f5ba4c66
高小川 <gaoxiaochuan@hotmail.com>
2019-01-27 23:13:50 +0800

    http web_gateway

--- http/web_gateway.c
+++ http/web_gateway.c
@@ -11,11 +11,7 @@ static void __web_resource(struct http_req_protocol * const req,
 void zl_webgateway_enter(struct http_req_protocol * const req,
                          struct http_res_protocol * const res)
 {
-    (void) req;
     zl_http_res_protocol_init(res);
-
     __web_resource(req, res);
-
-    /*zl_http_res_protocol_default_params(res);*/
-    info("enter web gateway");
+    zl_http_res_protocol_default_params(res);
 }
