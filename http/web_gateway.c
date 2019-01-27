cedc7c228b9a3a43aad67e1aee8ab137c2c188b7
高小川 <gaoxiaochuan@hotmail.com>
2019-01-28 01:01:10 +0800

    add base route method

--- http/web_gateway.c
+++ http/web_gateway.c
@@ -1,11 +1,19 @@
 #include "http/web_gateway.h"
 #include "debug/console.h"
 #include "lua_engine/caller.h"
+#include <string.h>
+#include <malloc.h>
 
 static void __web_resource(struct http_req_protocol * const req,
                            struct http_res_protocol * const res)
 {
-    zl_lua_engine_call("test/test.lua", req, res);
+    char * script_path = (char *) malloc(256);
+    strcpy(script_path, "./");
+    strcat(script_path, req->uri);
+
+    zl_lua_engine_call(script_path, req, res);
+
+    free(script_path);
 }
 
 void zl_webgateway_enter(struct http_req_protocol * const req,
