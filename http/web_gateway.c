a0fb40764e7dc2fe89a9d1b8af751765c0f8e3bb
高小川 <gaoxiaochuan@hotmail.com>
2019-02-19 19:17:14 +0800

    change webgateway (allow enter websocket)

--- http/web_gateway.c
+++ http/web_gateway.c
@@ -8,47 +8,46 @@
 #include <malloc.h>
 #include <unistd.h>
 
-static void __web_resource(struct http_req_protocol * const req,
-                           struct http_res_protocol * const res)
+static inline const char * __get_script(struct http_req_protocol * const req)
 {
     const char * script_path = zl_lua_engine_get_script_path(req);
-    if (script_path == NULL) {
-        error("script_path cannot got");
-        zl_lua_engine_notfound(res);
+    if (script_path != NULL && access(script_path, F_OK) == 0) {
+        return script_path;
     }
-    else {
-        if (access(script_path, F_OK) == 0) {
-            zl_lua_engine_call(script_path, req, res);
-        }
-        else {
-            zl_lua_engine_notfound(res);
-        }
+    if (script_path != NULL) {
         free((void *) script_path);
     }
+
+    return NULL;
 }
 
 static void __http(struct http_session * const session)
 {
-    if (zl_websocket_check(&session->req_protocol)) {
-        info("websocket handshake");
+    const char * script_path = __get_script(&session->req_protocol);
+    if (script_path == NULL) {
+        zl_lua_engine_notfound(&session->res_protocol);
+    }
+    else if (zl_websocket_check(&session->req_protocol)) {
         zl_websocket_accept(&session->req_protocol,
                             &session->res_protocol);
         session->is_websocket = true;
     }
     else {
-        info("normal http");
         zl_http_res_protocol_init(&session->res_protocol);
-        __web_resource(&session->req_protocol,
-                       &session->res_protocol);
+        zl_lua_engine_call(script_path, session);
     }
 
     zl_http_res_protocol_default_params(&session->res_protocol);
+    free((void *) script_path);
 }
 
 static void __websocket(struct http_session * const session)
 {
-    (void) session;
-    printf("WEBSOCKET\n");
+    const char * script_path = __get_script(&session->req_protocol);
+
+    zl_lua_engine_call(script_path, session);
+
+    free((void *) script_path);
 }
 
 void zl_webgateway_enter(struct http_session * const session)
